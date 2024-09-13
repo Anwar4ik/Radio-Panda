3ccfd4f2cea6fc7b2c1ff1ffac1178dc7ee1f58e
Anwar4ik <tim_anwar@mail.ru>
2024-09-13 16:18:02 +0500

    Update script.js
    корректировка воспроизведения аудио патока

--- script.js
+++ script.js
@@ -9,6 +9,7 @@ function toggleRadio() {
         button.classList.remove('pulse');
         button.classList.remove('playing');
     } else {
+        audio.load();  // Перезагружаем поток для продолжения прямого эфира
         audio.play();
         button.classList.add('pulse');
         button.classList.add('playing');
